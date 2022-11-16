b406bc84b2adac5edd62fbe781bc602e570f5c3c
Valentine <hzvalentine@gmail.com>
2022-11-16 19:57:36 +0100

    Store image data as bytes array.

--- src/main/java/ch/uzh/ifi/access/repository/TaskFileRepository.java
+++ src/main/java/ch/uzh/ifi/access/repository/TaskFileRepository.java
@@ -4,11 +4,13 @@ import ch.uzh.ifi.access.model.TaskFile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.access.prepost.PostFilter;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
 public interface TaskFileRepository extends JpaRepository<TaskFile, Long> {
 
+    @Transactional
     @PostFilter("hasRole(filterObject.task.assignment.course.url + '-assistant') or filterObject.published")
     List<TaskFile> findByTask_IdOrderByIdAscPathAsc(Long taskId);
 
