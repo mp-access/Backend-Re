61fdbd41af2e7941a784eb7ed9c136159192556c
simonhklaassen <simon.klaassen@outlook.com>
2025-04-14 14:54:56 +0200

    44 - adding example to the data model

---
+++ src/main/resources/db/migration/V3_0__add_example.sql
@@ -0,0 +1,19 @@
+ALTER TABLE task ADD COLUMN course_id bigint;
+ALTER TABLE task
+    ADD CONSTRAINT fk_task_course
+    FOREIGN KEY (course_id) REFERENCES course;
+
+ALTER TABLE task ALTER COLUMN run_command DROP NOT NULL;
+ALTER TABLE task ALTER COLUMN max_points DROP NOT NULL;
+ALTER TABLE task
+    ALTER COLUMN max_points SET DEFAULT 1;
+ALTER TABLE task ALTER COLUMN max_attempts DROP NOT NULL;
+ALTER TABLE task
+    ALTER COLUMN max_attempts SET DEFAULT 1;
+ALTER TABLE task ALTER COLUMN assignment_id DROP NOT NULL;
+
+ALTER TABLE task
+    ADD CONSTRAINT assignment_or_course_reference CHECK (
+    (assignment_id is not null and course_id is null) or
+    (assignment_id is null and course_id is not null)
+);
