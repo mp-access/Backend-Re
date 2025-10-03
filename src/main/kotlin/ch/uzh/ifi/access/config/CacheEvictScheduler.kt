c89ada9ccaf4602aea6041e3eda1a0011c89768e
Patric Salvisberg <patric.salvisberg@gmail.com>
2025-10-03 09:23:57 +0200

    added global cache eviction

--- src/main/kotlin/ch/uzh/ifi/access/config/CacheEvictScheduler.kt
+++ src/main/kotlin/ch/uzh/ifi/access/config/CacheEvictScheduler.kt
@@ -1,14 +1,16 @@
 package ch.uzh.ifi.access.config
 
 import io.github.oshai.kotlinlogging.KotlinLogging
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.cache.CacheManager
 import org.springframework.cache.annotation.CacheEvict
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
 @Component
-class CacheEvictScheduler {
+class CacheEvictScheduler(private val cacheManager: CacheManager,  @Value("\${caching.eviction-rate}") private val evictionRate: String) {
     private val logger = KotlinLogging.logger {}
-    @Scheduled(fixedRate = 15000, ) // every 15s
+    @Scheduled(fixedRateString = "15s", )
     @CacheEvict(
         value = ["ExampleService.getInteractiveExampleSlug"],
         allEntries = true
@@ -16,4 +18,16 @@ class CacheEvictScheduler {
     fun evictInteractiveExampleSlugCache() {
         logger.info{"Scheduled cache eviction of ExampleService.getInteractiveExampleSlug" }
     }
+
+    @Scheduled(fixedRateString = "\${caching.eviction-rate}")
+    fun evictAllCaches() {
+        logger.info { "Starting global cache eviction..." }
+
+        cacheManager.cacheNames.forEach { cacheName ->
+            cacheManager.getCache(cacheName)?.clear()
+        }
+
+        logger.info{"Finished global cache eviction."}
+    }
+
 }
